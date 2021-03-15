084fc28de4176ce59d190d3f364f02d8f44e44a7
zEreal23 <zEreal23@hotmail.com>
2021-03-16 00:55:45 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import 'antd/dist/antd.css';
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
