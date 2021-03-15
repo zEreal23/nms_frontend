a0c4d246e783f803a14e1bb8067242c200370169
zEreal23 <zEreal23@hotmail.com>
2021-03-16 01:07:51 +0700

    menuPage

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Card, Row, Col, Tabs} from 'antd';
-import Carousel from 'react-elastic-carousel';
+
 import Slider from 'react-slick';
 
 import p1 from '../image/p1.jpg';
@@ -20,11 +20,38 @@ const breakPoints = [
 
 const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-};
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
 const promotionStyle = {
     height: '160px',
@@ -142,27 +169,21 @@ const Home = ({match}) => {
     );
 
     return (
-        <div className="container" style={{marginTop: 20}}>
+        <div style={{marginTop: 20}}>
             <h1>Table No. {noTable.name}</h1>
             <div className="App">
                 <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
+                    <div style={{display:'flex',justifyContent:'center',alignItems: 'center', width: '100%',margin: 10}}>
+                        <img style={{width: '100%',height:'auto',objectFit:'contain'}} src={p1} alt="p1"/>
                     </div>
-                    <div>
-                        <h3>2</h3>
+                    <div style={{display:'flex',justifyContent:'center',alignItems: 'center', width: '100%'}}>
+                        <img style={{width: '100%',height:'auto',objectFit:'contain'}} src={p1} alt="p1"/>
                     </div>
-                    <div>
-                        <h3>3</h3>
+                    <div style={{display:'flex',justifyContent:'center',alignItems: 'center', width: '100%'}}>
+                        <img style={{width: '100%',height:'auto',objectFit:'contain'}} src={p1} alt="p1"/>
                     </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
+                    <div style={{display:'flex',justifyContent:'center',alignItems: 'center', width: '100%'}}>
+                        <img style={{width: '100%',height:'auto',objectFit:'contain'}} src={p1} alt="p1"/>
                     </div>
                 </Slider>
             </div>
